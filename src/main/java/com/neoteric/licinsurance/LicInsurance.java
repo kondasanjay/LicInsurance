479666f6841966e40fce031929baa3aadc20539c
sanja <sanjay.konda24@gmail.com>
2024-07-06 15:01:45 +0530

    lic

---
+++ src/main/java/com/neoteric/licinsurance/LicInsurance.java
@@ -0,0 +1,22 @@
+package com.neoteric.licinsurance;
+
+public class LicInsurance {
+
+   public String FullName;
+  public int  Dob;
+    public String Gender;
+    public String MaritalStatus;
+    public String Address;
+    public long PhoneNumber;
+    public String EmailId;
+    public String EmployeName;
+    public int AnnualIncome;
+    public String PlanType;
+    public String NomineName;
+    public int NomineNameDob;
+    public String NomineNameGender;
+    public boolean PreExistingCondition;
+    public String PaymentDetails;
+
+
+}
---
+++ src/test/java/com/neoteric/licinsurance/LIcInsuranceTest.java
@@ -0,0 +1,38 @@
+package com.neoteric.licinsurance;
+
+import org.junit.jupiter.api.Test;
+
+public class LIcInsuranceTest {
+
+
+    @Test
+
+    public void test(){
+
+
+        LicInsurance customer1=new  LicInsurance();
+
+        customer1.FullName="sanjay";
+        customer1.Dob=22-2-2000;
+        customer1.Gender="male";
+        customer1.MaritalStatus="yes";
+        customer1.Address="1-1 Nehru nagar";
+        customer1.PhoneNumber=8877665544l;
+        customer1.EmailId="sanjay.konda24@gmail.com";
+        customer1.EmployeName="raju";
+        customer1.AnnualIncome=223333;
+        customer1.PlanType="basic";
+        customer1.NomineName="rahul";
+        customer1.NomineNameDob=22-2-2002;
+        customer1.NomineNameGender="male";
+        customer1.PreExistingCondition=true;
+        customer1.PaymentDetails="UPI via";
+
+
+
+
+
+
+    }
+
+}
